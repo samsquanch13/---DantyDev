3a557f51b9235236ff11b403348f26305138b1f0
samsquanch13 <d.dainty@protonmail.com>
2021-03-29 16:25:48 -0400

    finished chapter 2 and basic HTML tags

--- An Easier Way to Learn JS/asmarterway.js
+++ An Easier Way to Learn JS/asmarterway.js
@@ -1,4 +1,15 @@
 // this is an alert
 
-alert("yo, this is an alert!");
+alert("yo, this is an alert!")
 
+var name = "Samsquanch";
+console.log(name);
+
+var name = "Danty";
+console.log(name);
+
+var thx = "thanks for being so cool!";
+alert(thx);
+
+var empty;
+empty = "now I'm full! OwO";
